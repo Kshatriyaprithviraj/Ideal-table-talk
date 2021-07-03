b0fe61ebc8f1b2424379b019af06888b408fb0c8
kshatriyaprithviraj <1999prithviraj1999@gmail.com>
2021-07-03 20:59:26 +0530

    setup express server, router and dependable

---
+++ container.js
@@ -0,0 +1,21 @@
+const dependable = require('dependable');
+const path = require('path');
+
+const container = dependable.container();
+
+const simpleDependencies = [['_', 'loadash']];
+
+simpleDependencies.forEach(function (value) {
+  container.register(value[0], function () {
+    return require(value[1]);
+  });
+});
+
+container.load(path.join(__dirname, '/controllers'));
+container.load(path.join(__dirname, '/helpers'));
+
+container.register('container', function () {
+  return container;
+});
+
+module.exports = container;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+const port = 3000;
+
+container.resolve(function (users) {
+  const app = SetupExpress();
+
+  function SetupExpress() {
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(port, function () {
+      console.log(`Server listening on port ${port}`);
+    });
+    ConfigureExpress(app);
+
+    /**
+     * Settting up a express router.
+     * A simple wrapper for Express's Router that allows middleware
+     * to return promises. This package makes it simpler to write
+     * route handlers for Express when dealing with promises by
+     * reducing duplicate code.
+     */
+
+    const router = require('express-promise-router')();
+    users.SetRouting();
+    app.use(router);
+  }
+
+  function ConfigureExpress(app) {
+    app.use(express.static('public'));
+    app.set('view engine', 'ejs');
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+  }
+});
