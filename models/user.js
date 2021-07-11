5edfa31fb4423c7c7c81b3eac689cb5137e82b12
Prithvi <1999prithviraj1999@gmail.com>
2021-07-12 02:18:57 +0530

    Dev (#10)
    * fixed ejs error and added public,css,js folders
    
    * fixed minor errors
    
    * added basic styling for signup page and created user schema , model
    
    * encrypt passwords and signup post route

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
+
 const userSchema = mongoose.Schema({
   email: {
     type: String,
@@ -35,4 +38,14 @@ const userSchema = mongoose.Schema({
   googleTokens: Array,
 });
 
-module.exports = mongoose.model('User', userSchemar);
+
+userSchema.methods.encryptPassword = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+userSchema.method.validUserPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const ejs = require('ejs');
 const http = require('http');
@@ -53,6 +54,7 @@ container.resolve(function (users) {
   }
 
   function ConfigureExpress(app) {
+    require('./passport/passport-local');
     app.use(express.static('public'));
     app.use(cookieParser());
     app.set('view engine', 'ejs');
